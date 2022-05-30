76e6378487315e963b7b6033b7d05f4ebcd8a674
Vlad Yanum <Y5280@eon.com>
2022-05-30 12:29:22 +0200

    Remove scm config (obsolete)

--- Modules/app_v2/main.tf
+++ Modules/app_v2/main.tf
@@ -19,13 +19,4 @@ resource "azurerm_app_service" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   app_service_plan_id = azurerm_app_service_plan.test.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-  }
-
 }
