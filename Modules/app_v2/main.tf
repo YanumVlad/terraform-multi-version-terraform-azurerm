4a9890b698404f2669f4a8251a739f312baed83f
Vlad Yanum <Y5280@eon.com>
2022-05-30 12:55:03 +0200

    remove scm ref

--- Modules/app_v2/main.tf
+++ Modules/app_v2/main.tf
@@ -20,11 +20,6 @@ resource "azurerm_app_service" "test" {
   resource_group_name = azurerm_resource_group.test.name
   app_service_plan_id = azurerm_app_service_plan.test.id
 
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
   app_settings = {
   }
 
